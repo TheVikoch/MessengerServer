18390f39753e10b0dbadb6441093d86b7a1dea1c
viac5 <adronnyi.kollaider@gmail.com>
2026-02-28 17:33:49 +0300

    Test Controller + bug fix

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MessengerServer.Controllers
 {
@@ -17,6 +18,23 @@ namespace MessengerServer.Controllers
         {
             throw new Exception("Тестовая ошибка");
         }
+
+        [HttpGet("protected")]
+        [Authorize]
+        public IActionResult Protected()
+        {
+            var userId = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+            var email = User.Identity?.Name;
+        
+            return Ok(new { message = "You have access to protected resource!", userId, email });
+        }
+
+        [HttpGet("public")]
+        public IActionResult Public()
+        {
+            return Ok(new { message = "This is a public endpoint" });
+        }
+
     }
 }
 
